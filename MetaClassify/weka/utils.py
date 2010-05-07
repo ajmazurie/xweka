c6616373cf35e2546bddb7c65e2666276247c909
Aurelien Mazurie <ajmazurie@oenone.net>
2010-05-07 15:07:49 -0600

    Release 0.4 (April 3, 2006)

---
+++ MetaClassify/weka/utils.py
@@ -0,0 +1,34 @@
+# Miscellaneous methods
+
+import os
+
+class TextReader:
+
+	def __init__ (self, file, split = True):
+		if (not os.path.exists(file)):
+			print >>sys.stderr, " Error : file '%s' not found." % file
+			sys.exit(1)
+
+		self.source = open(file, 'r')
+		self.split = split
+
+	def __iter__ (self):
+		return self
+
+	def next (self):
+		while True:
+			line = self.source.readline()
+
+			if (line == ''):
+				self.source.close()
+				raise StopIteration
+
+			line = line.strip()
+
+			if (line == '') or (line[0] == '#'):
+				continue
+
+			if self.split:
+				return line.split('	')
+			else:
+				return line
