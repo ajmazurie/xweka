081dd3f10640505222622daad42b2682860d53e4
Aurelien Mazurie <ajmazurie@oenone.net>
2011-07-14 16:55:55 -0600

    Added: license, changes and version file

--- xweka/__init__.py
+++ xweka/__init__.py
@@ -1,8 +1,8 @@
 
 __version_major__ = 0
 __version_minor__ = 2
-__revision__ = 0
-__build__ = None
+__revision__ = 1
+__build__ = "211DCEC"
 
 version = "%s.%s (revision %s, build %s)" % (
 	__version_major__,
